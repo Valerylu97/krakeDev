60131bfee1d28acb6cbd44c4c7824a557769db94
Valerylu97 <valerylu95@gmail.com>
2025-09-23 14:53:03 -0500

    funcion recuperar Int y Float completas

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -3,6 +3,10 @@ saludar=function(){
     let nombreIngresado = recuperarTexto("txtNombre");
     //recuperar el valor de la caja de texto txtApellido
     let apellidoIngresado = recuperarTexto("txtApellido");
+    //recuperar el valor de la caja de texto txtEdad
+    let edadIngresada = recuperarInt("txtEdad");    
+    //recuperar el valor de la caja de texto txtEstatura
+    let estaturaIngresada = recuperarFloat("txtEstatura");
     //mostrar un mensaje de saludo
     alert("Hola " + nombreIngresado + " " + apellidoIngresado + ", bienvenido a KrakeDev");
 }
@@ -11,4 +15,16 @@ recuperarTexto = function(idComponente){
     let componente = document.getElementById(idComponente);
     let valorIngresado = componente.value;
     return valorIngresado;
+}
+
+recuperarInt = function(idComponente){
+    let componente = recuperarTexto(idComponente);
+    let valorEntero = parseInt(componente);
+    return valorEntero;
+}
+
+recuperarFloat = function(idComponente){
+    let componente = recuperarTexto(idComponente);
+    let valorFloat = parseFloat(componente);
+    return valorFloat;
 }
