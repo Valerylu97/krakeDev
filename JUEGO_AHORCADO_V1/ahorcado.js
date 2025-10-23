9c5173bd5f3fd4f6abf2d416f7e67c280c764286
Valerylu97 <valerylu95@gmail.com>
2025-10-23 09:51:28 -0500

    PASO 2

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -31,3 +31,21 @@ guardarPalabra = function(){
         alert("DEBE INGRESAR UNA PALABRA DE 5 LETRAS MAYUSCULAS")
     }   
 }
+
+mostrarLetra = function(letra, posicion){
+    if(posicion == 0){
+        mostrarTexto("div0", letra);
+    }
+    if(posicion == 1){
+        mostrarTexto("div1", letra);
+    }
+    if(posicion == 2){
+        mostrarTexto("div2", letra);
+    }
+    if(posicion == 3){
+        mostrarTexto("div3", letra);
+    }
+    if(posicion == 4){
+        mostrarTexto("div4", letra);
+    }
+}
